1e79cccfc44b52dd6361bdd813bf34fcb1a46baf
Marcin Kraszewski <mrkraszewski@gmail.com>
2023-06-14 00:29:55 +0200

    finished gallery POC

--- app.py
+++ app.py
@@ -10,11 +10,20 @@ from wtforms.validators import DataRequired, Email
 from config import Config
 from forms.samonosne_form import SamonosneStairsForm
 from forms.beton_form import BetonStairsForm
-from utils.products import products as images_data
-
+from utils.products import products as image_links
+from collections import defaultdict
 
 app = Flask(__name__)
 
+def filter_valid_images(products):
+    for category in products:
+        for product in products[category]:
+            images = [img for img in product["images"] if os.path.isfile(os.path.join(app.root_path, 'static', img))]
+            if len(images) >= 1:
+                product["images"] = images
+    return products
+
+image_links = filter_valid_images(image_links)
 
 app.config.from_object(Config)
 
@@ -67,15 +76,11 @@ def login():
             login_user(user)
             return redirect(url_for('protected'))
         else:
-            return 'Invalid username or password'
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
     else:
-        return """
-            <form method="post">
-                Username: <input type="text" name="username"><br>
-                Password: <input type="password" name="password"><br>
-                <input type="submit" value="Submit">
-            </form>
-            """
+        return render_template('login.html')
+
 
 
 @app.route('/logout')
@@ -108,10 +113,19 @@ def home():
     return render_template('index.html', site_title=site_title)
 
 
+import random
+import os
+
+import random
+
+import random
+
+import random
+
+import random
+
 @app.route('/galeria')
 def galeria():
-    images = os.listdir(os.path.join(app.static_folder, "images/products/"))
-
     PORTFOLIO_CATEGORIES = {
         "schody": {
             "schody_na_beton": True,
@@ -125,9 +139,30 @@ def galeria():
         }
     }
 
+    # Select only the categories that should be published
     categories_to_publish = {k: [sub_k for sub_k, sub_v in v.items() if sub_v] for k, v in PORTFOLIO_CATEGORIES.items()}
 
-    return render_template('galeria.html', images=images, title='Galeria', categories=categories_to_publish)
+    subcategory_images = {}
+    category_used_images = {}
+
+    for category, subcategories in categories_to_publish.items():
+        category_images = []  # Initialize with an empty list
+        for subcategory in subcategories:
+            subcategory_products = image_links.get(subcategory, [])
+            if subcategory_products:
+                # Exclude images already used for categories
+                subcategory_products = [product for product in subcategory_products if product['images'][0] not in category_used_images.values()]
+                if subcategory_products:
+                    random_product = random.choice(subcategory_products)
+                    subcategory_images[subcategory] = random_product['images'][0]  # Assuming every product has at least one image
+                    category_images.append(random_product['images'][0])  # Add image to category's list of images
+
+        if category_images:
+            category_image = random.choice(category_images)
+            category_used_images[category] = category_image  # Save the image used for the category
+            category_images.remove(category_image)  # Remove the image from the category's list
+
+    return render_template('galeria.html', title='Galeria', categories=categories_to_publish, category_images=category_used_images, subcategory_images=subcategory_images)
 
 
 @app.route('/oferta')
@@ -198,16 +233,32 @@ def cennik_beton():
 
 @app.route('/portfolio/<category>')
 def category(category):
-    products = [product for product in images_data[category]]
-    category_links = []
+    products = [product for product in image_links[category]]
+    photos_in_category = []
     for product in products:
-        product_desc = product['description']
-        image_links = product['images']
-        image_link = f"{image_links[0]}"
-        category_links.append(image_link)
+        try:
+            product_desc = product['description']
+            product_image_links = [link for link in product['images']]  # Remove 'static/' from image paths
+            this_product = {'description': product_desc, 'images': product_image_links}
+            photos_in_category.append(this_product)
+        except Exception as ee:
+            print(ee)
 
-    return render_template('gallery_view.html', images=category_links, count=len(category_links))
+    print(f"Single 'image' item: {photos_in_category[0]}")
+    return render_template('gallery_view.html', images=photos_in_category)
 
+
+
+@app.route('/portfolio/<category>/<product>')
+def product(category, product):
+    # Assume image_links is a dictionary that maps category names to lists of product dictionaries.
+    # Find the correct product based on the provided category and product description.
+    for product_dict in image_links[category]:
+        if product_dict['description'] == product:
+            return render_template('product_view.html', images=product_dict['images'], description=product)
+    
+    # If the product wasn't found, return a 404 error.
+    return "Product not found", 404
 
 if __name__ == "__main__":
     with app.app_context():
