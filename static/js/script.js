aec955da6828940b0c55529eb3e47ac9b3210d12
Marcin Kraszewski <mrkraszewski@gmail.com>
2023-06-18 13:42:49 +0200

    gallery works as intended (3 cols/click highlight)

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,9 @@
-var lang = 'pl'; // Initial language
-var elements = document.querySelectorAll('[data-en]');
-var button = document.getElementById('switch-lang');
+$(document).ready(function() {
+  $(".product-img").click(function() {
+    var index = $(this).data("index");
+    $("#carouselExampleIndicators").carousel(index);
 
-function switchLang() {
-  lang = (lang == 'pl') ? 'pl' : 'en';
-  button.textContent = (lang == 'pl') ? 'PL' : 'EN';
-  elements.forEach(function(element) {
-    element.textContent = element.dataset[lang];
+    $(".product-img").removeClass("active").css("opacity", "0.5");
+    $(this).addClass("active").css("opacity", "1");
   });
-}
-
-button.addEventListener('click', switchLang);
+});
